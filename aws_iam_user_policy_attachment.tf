6326afee45b746cff60da1db001d9bfe999b8604
Gregory Mirsky <gmirsky@gmail.com>
2021-06-04 09:45:15 -0400

    changed parm to look up resource output

--- aws_iam_user_policy_attachment.tf
+++ aws_iam_user_policy_attachment.tf
@@ -1,4 +1,4 @@
 resource "aws_iam_user_policy_attachment" "snowflake_user" {
-  user       = local.snowflake_s3_user
+  user       = aws_iam_user.snowflake_user.name
   policy_arn = aws_iam_policy.snowflake_user.arn
 }
